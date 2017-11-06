cc9456cfe9dfe7436a60e8b95a80f94a934a9778
kiatAWDSA <lskiat@huskers.unl.edu>
2017-11-06 17:37:41 -0600

    Initial commit

---
+++ HIH8000_I2C.cpp
@@ -0,0 +1,99 @@
+/********************************************************************** 
+  Obtain measurements from a Honeywell HIH8000 Series sensor connected to an
+  Arduino board using I2C protocol (not SPI):
+  https://sensing.honeywell.com/sensors/humidity-sensors/HIH8000-series
+  
+  To adjust the internal settings of the
+  sensor such as its I2C address and alarms, use the HIH8000CommandI2C library instead:
+  https://github.com/kiatAWDSA/HIH8000CommandI2C
+  
+  Please note that the pins used for I2C communication are different for each
+  Arduino board. For Arduino Uno, the SDA and SCL pins are A4 and A5, respectively.
+  A list of boards and corresponding pins can be found at:
+  https://www.arduino.cc/en/Reference/Wire
+  
+  
+  Copyright 2017 Soon Kiat Lau
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+***********************************************************************/
+
+#include "HIH8000_I2C.h"
+
+// Instantiate with device address.
+HIH8000_I2C::HIH8000_I2C(uint8_t address) {
+  address_ = address;
+}
+
+// If user prefers to instantiate class first and set the address later.
+HIH8000_I2C::HIH8000_I2C() {}
+
+// No specific destruction subroutines necessary (ending Wire here might affect other devices on I2C line).
+HIH8000_I2C::~HIH8000_I2C() {}
+
+// Set/change I2C address of the sensor
+void HIH8000_I2C::setAddress(uint8_t address) {
+  address_ = address;
+}
+
+// To initiate a measurement, need to send a write request to the sensor without any data. This is equivalent to initiating a transmission in the Wire libary.
+// NOTE: It typically takes 36.65 ms for a reading to be generated.
+void HIH8000_I2C::triggerMeasurement() {
+  Wire.beginTransmission(address_);
+  Wire.endTransmission();
+}
+
+// Get the data stored in the sensor's registers and process them. These data is from the most recent triggered measurement.
+void HIH8000_I2C::fetchMeasurement(){
+  Wire.requestFrom(address_, dataBytes_);
+  
+  // Get raw humidity data
+  humidityBuffer_ = Wire.read();
+  humidityBuffer_ <<= 8;
+  humidityBuffer_ |= Wire.read();
+
+  // Get raw temperature data
+  temperatureBuffer_ = Wire.read();
+  temperatureBuffer_ <<= 8;
+  temperatureBuffer_ |= Wire.read();
+  temperatureBuffer_ >>= 2;  // Remove the last two "Do Not Care" bits
+
+  // Extract out status bits and clean raw humidity data
+  status_ = 0xC000 & humidityBuffer_;         // The integer constant is 1100000000000000 in binary
+  status_ >>= 14;                             // Push it to become 2 digits
+  humidityBuffer_ = 0x3FFF & humidityBuffer_; // The integer constant is 0011111111111111 in binary
+  
+  // Process the raw values. 16382 is 2^14 - 2.
+  humidity_ = humidityBuffer_/16382.0 * 100;
+  temperature_ = temperatureBuffer_/16382.0 * 165 - 40;
+}
+
+// Get the address set for the sensor.
+uint8_t HIH8000_I2C::getAddress() {
+  return address_;
+}
+
+// Get the status code given by the sensor.
+uint8_t HIH8000_I2C::getStatus() {
+  return status_;
+}
+
+// Get the processed humidity value.
+float HIH8000_I2C::getHumidity() {
+  return humidity_;
+}
+
+// Get the processed temperature value.
+float HIH8000_I2C::getTemperature() {
+  return temperature_;
+}
---
+++ HIH8000_I2C.h
@@ -0,0 +1,63 @@
+/********************************************************************** 
+  Obtain measurements from a Honeywell HIH8000 Series sensor connected to an
+  Arduino board using I2C protocol (not SPI):
+  https://sensing.honeywell.com/sensors/humidity-sensors/HIH8000-series
+  
+  To adjust the internal settings of the
+  sensor such as its I2C address and alarms, use the HIH8000CommandI2C library instead:
+  https://github.com/kiatAWDSA/HIH8000CommandI2C
+  
+  Please note that the pins used for I2C communication are different for each
+  Arduino board. For Arduino Uno, the SDA and SCL pins are A4 and A5, respectively.
+  A list of boards and corresponding pins can be found at:
+  https://www.arduino.cc/en/Reference/Wire
+  
+  
+  Copyright 2017 Soon Kiat Lau
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+***********************************************************************/
+
+#ifndef HIH8000_I2C_H
+#define HIH8000_I2C_H
+
+#include "Arduino.h"
+#include "Wire.h"
+
+class HIH8000_I2C  {
+  public:
+    HIH8000_I2C(uint8_t address);
+    HIH8000_I2C();
+    ~HIH8000_I2C();
+    void setAddress(uint8_t address);
+    void triggerMeasurement();
+    void fetchMeasurement();
+    uint8_t getStatus();
+    uint8_t getAddress();
+    float getHumidity();
+    float getTemperature();
+  
+  private:
+    // This is used when fetching data. The documentation says 4 bytes will be transmitted from the sensor.
+    const uint8_t dataBytes_ = 4;
+  
+    bool addressSet_;
+    uint8_t address_;
+    uint8_t status_;
+    uint16_t humidityBuffer_;
+    uint16_t temperatureBuffer_;
+    float humidity_;
+    float temperature_;
+};
+
+#endif  // HIH8000_I2C_H
